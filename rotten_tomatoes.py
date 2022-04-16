6d0375f938e446413fc7916b4ca7a92603b496af
KD131 <71063280+KD131@users.noreply.github.com>
2022-04-16 22:25:50 +0200

    prints all movie titles from list by year with threadpool

--- rotten_tomatoes.py
+++ rotten_tomatoes.py
@@ -1,4 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor
 import bs4
+import pandas as pd
 import requests
 
 
@@ -14,5 +16,22 @@ def getMovieLinks(year):
     elems = soup.select('#top_movies_main a.articleLink')
     return [base + e.get('href') for e in elems if e.get('href') and e.get('href').startswith('/')]
 
+def getMovie(link):
+    r = requests.get(link)
+    r.raise_for_status()
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
+    title = soup.select_one('.scoreboard__title').contents
+    print(title)
+
+def getMovies(links, workers=5):
+    movies = []
+    with ThreadPoolExecutor(workers) as exec:
+        exec.map(getMovie, links)
+
+    # for link in links:
+    #     getMovie(link)
+
 if __name__ == '__main__':
     links = getMovieLinks(2020)
+    getMovies(links)
