cdcd78f0b64f17f5ac14a1b9e837ed9f0af3e39d
KD131 <71063280+KD131@users.noreply.github.com>
2022-04-17 03:14:08 +0200

    selenium try, do NOT open up a browser for each link. BAD.

--- rotten_tomatoes.py
+++ rotten_tomatoes.py
@@ -1,7 +1,12 @@
 from concurrent.futures import ThreadPoolExecutor
+from time import sleep, time
+
 import bs4
 import pandas as pd
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 
 def getMovieLinks(year):
@@ -17,29 +22,46 @@ def getMovieLinks(year):
     return [base + e.get('href') for e in elems if e.get('href') and e.get('href').startswith('/')]
 
 def getMovie(link):
-    r = requests.get(link)
-    r.raise_for_status()
-    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    # r = requests.get(link)
+    # r.raise_for_status()
+    # soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    options = Options()
+    options.headless = True
+    browser = webdriver.Firefox(options=options)
+    browser.get(link)
 
-    title = soup.select_one('.scoreboard__title').text
-    info = soup.select_one('.scoreboard__info').text
+    scoreboard = browser.find_element(By.CSS_SELECTOR, 'score-board.scoreboard')
+    # print(scoreboard.text)
+    # shadow = scoreboard.shadow_root     # would work in a chromium browser
+    shadow = browser.execute_script('return arguments[0].shadowRoot', scoreboard)
+    # this doesn't work for some reason
+
+    # Running many browsers crashes the whole thing.
+    # And overfills the RAM which then fills up the C drive which is real bad.
+    # I think I'll have to run a single browser, and it'll just take a while.
+    # Then I'll save it to a CSV.
+    # And I'll need to run it in Chrome so I'll have to install the webdriver.
+
+    title = scoreboard.find_element(By.CSS_SELECTOR, '.scoreboard__title').text
+    print(title)
+    info = scoreboard.find_element(By.CSS_SELECTOR, '.scoreboard__info').text
     split = info.split(', ')
     genres = split[1].split('/')
     runtime = pd.Timedelta(split[2])        # Could also use datetime.timedelta. Maybe parse the string datetime.datetime.strptime or dateutil.parser
-    tomato = soup.select_one('.tomatometer-container .percentage').text
+    tomato = shadow.find_element(By.CSS_SELECTOR, '.tomatometer-container .percentage').text
     tomato = tomato[:-1]                    # removes last char which is '%'. If needed, can also strip but unnecessary here.
-    audience = soup.select_one('.audience-container .percentage').text
+    audience = shadow.find_element(By.CSS_SELECTOR, '.audience-container .percentage').text
     audience = audience[:-1]
 
     return [title, genres, runtime, tomato, audience]
 
 def getMovies(links, workers=5):
-    movies = []
     with ThreadPoolExecutor(workers) as exec:
         return exec.map(getMovie, links)
 
 if __name__ == '__main__':
-    links = getMovieLinks(2020)
-    data = getMovies(links)
-    df = pd.DataFrame(data, columns=['Title', 'Genres', 'Runtime', 'Tomato %', 'Audience %'])
-    print(df.head)
+    print(getMovie('https://www.rottentomatoes.com/m/yes_god_yes'))
+    # links = getMovieLinks(2020)
+    # data = getMovies(links)
+    # df = pd.DataFrame(data, columns=['Title', 'Genres', 'Runtime', 'Tomato %', 'Audience %'])
+    # print(df.head)
